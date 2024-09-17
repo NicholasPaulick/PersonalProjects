6878896b5484d3e3c8cc8150c6e2e4cef609f38d
Nicholas Paulick <paulicknicholas@gmail.com>
2024-09-16 19:47:07 -0500

    Improved Tron
    +Added Menus
    +Fixed Collisions

--- Easy/Tron-Clone/main.cpp
+++ Easy/Tron-Clone/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <vector>
 #include <deque>
 #include <algorithm>
@@ -8,11 +9,14 @@ const int WINDOW_HEIGHT = 600;
 const int PLAYER_SPEED = 5;
 const int MAX_TRAIL_LENGTH = 1000;
 
+enum GameState { MENU, PLAYING, PAUSED, GAMEOVER };
+
 struct Player {
     SDL_Rect rect;
     SDL_Point direction;
     std::deque<SDL_Rect> trail;
     int health;
+    SDL_Color color;
 };
 
 bool checkCollision(const SDL_Rect& a, const SDL_Rect& b) {
@@ -20,32 +24,35 @@ bool checkCollision(const SDL_Rect& a, const SDL_Rect& b) {
 }
 
 bool checkTrailCollision(const Player& player, const std::deque<SDL_Rect>& trail) {
-    SDL_Rect playerHead = { player.rect.x, player.rect.y, player.rect.w, player.rect.h };
+    SDL_Rect playerHead = { player.rect.x + player.direction.x * player.rect.w / 2, 
+                             player.rect.y + player.direction.y * player.rect.h / 2,
+                             player.rect.w / 2, 
+                             player.rect.h / 2 };
+    int count = 0;
     for (const auto& rect : trail) {
-        if (checkCollision(playerHead, rect)) {
-            return true;
+        if (count != 0){
+            if (checkCollision(playerHead, rect)) {
+                return true;
+            }
         }
+        count++;
     }
     return false;
 }
 
 void updatePlayerPosition(Player& player) {
-    player.rect.x += player.direction.x * PLAYER_SPEED;
-    player.rect.y += player.direction.y * PLAYER_SPEED;
+    int newX = player.rect.x + player.direction.x * PLAYER_SPEED;
+    int newY = player.rect.y + player.direction.y * PLAYER_SPEED;
 
     // Wall collision
-    if (player.rect.x < 0) {
-        player.rect.x = 0;
+    if (newX < 0 || newX + player.rect.w > WINDOW_WIDTH ||
+        newY < 0 || newY + player.rect.h > WINDOW_HEIGHT) {
+        player.health = 0;
+        return;
     }
-    if (player.rect.x + player.rect.w > WINDOW_WIDTH) {
-        player.rect.x = WINDOW_WIDTH - player.rect.w;
-    }
-    if (player.rect.y < 0) {
-        player.rect.y = 0;
-    }
-    if (player.rect.y + player.rect.h > WINDOW_HEIGHT) {
-        player.rect.y = WINDOW_HEIGHT - player.rect.h;
-    }
+
+    player.rect.x = newX;
+    player.rect.y = newY;
 
     // Update trail
     player.trail.push_front(player.rect);
@@ -55,16 +62,42 @@ void updatePlayerPosition(Player& player) {
 
     // Check for self-collision (excluding the latest trail segment)
     for (size_t i = 1; i < player.trail.size(); ++i) {
-        if (checkCollision(player.rect, player.trail[i])) {
+        if (checkTrailCollision(player, player.trail)) {
             player.health = 0;
             break;
         }
     }
 }
 
+void renderText(SDL_Renderer* renderer, const char* text, int x, int y, SDL_Color color, TTF_Font* font) {
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text, color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect textRect = { x, y, surface->w, surface->h };
+    SDL_RenderCopy(renderer, texture, nullptr, &textRect);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
+
+void renderStartMenu(SDL_Renderer* renderer, TTF_Font* font) {
+    SDL_Color color = { 255, 255, 255, 255 };
+    renderText(renderer, "TRON GAME", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 50, color, font);
+    renderText(renderer, "Press SPACE to Start", WINDOW_WIDTH / 2 - 150, WINDOW_HEIGHT / 2, color, font);
+}
+
+void renderPauseMenu(SDL_Renderer* renderer, TTF_Font* font) {
+    SDL_Color color = { 255, 255, 255, 255 };
+    renderText(renderer, "PAUSED", WINDOW_WIDTH / 2 - 50, WINDOW_HEIGHT / 2 - 50, color, font);
+    renderText(renderer, "Press P to Resume", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2, color, font);
+}
+
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (TTF_Init() == -1) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "TTF could not initialize! TTF_Error: %s\n", TTF_GetError());
         return 1;
     }
 
@@ -80,73 +113,117 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Player player1, player2;
-    player1.rect = { 100, 300, 5, 5 };
-    player1.direction = { 1, 0 };
-    player1.health = 100;
+    TTF_Font* font = TTF_OpenFont("SUSE-VariableFont_wght.ttf", 24);
+    if (font == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Font could not be loaded! TTF_Error: %s\n", TTF_GetError());
+        return 1;
+    }
 
-    player2.rect = { 700, 300, 5, 5 };
-    player2.direction = { -1, 0 };
-    player2.health = 100;
+    Player player1 = { { 100, 300, 10, 10 }, { 1, 0 }, {}, 100, { 0, 0, 255, 255 } };
+    Player player2 = { { 700, 300, 10, 10 }, { -1, 0 }, {}, 100, { 255, 0, 0, 255 } };
 
     bool quit = false;
     SDL_Event event;
+    GameState gameState = MENU;
 
     Uint32 lastTime = SDL_GetTicks();
+    float deltaTime = 0.0f;
 
     while (!quit) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
+
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_SPACE:
+                        if (gameState == MENU) {
+                            gameState = PLAYING;
+                        } else if (gameState == GAMEOVER) {
+                            // Reset the game
+                            player1 = { { 100, 300, 10, 10 }, { 1, 0 }, {}, 100, { 0, 0, 255, 255 } };
+                            player2 = { { 700, 300, 10, 10 }, { -1, 0 }, {}, 100, { 255, 0, 0, 255 } };
+                            gameState = PLAYING;
+                        }
+                        break;
+                    case SDLK_p:
+                        if (gameState == PLAYING) {
+                            gameState = PAUSED;
+                        } else if (gameState == PAUSED) {
+                            gameState = PLAYING;
+                        }
+                        break;
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                }
+            }
         }
 
         Uint32 currentTime = SDL_GetTicks();
-        float deltaTime = (currentTime - lastTime) / 1000.0f;
+        deltaTime = (currentTime - lastTime) / 1000.0f;
         lastTime = currentTime;
 
-        // Update player positions and self-collision checks
-        updatePlayerPosition(player1);
-        updatePlayerPosition(player2);
+        if (gameState == PLAYING) {
+            // Update player positions
+            updatePlayerPosition(player1);
+            updatePlayerPosition(player2);
 
-        // Check inter-player collisions and wall collisions
-        if ((player1.health <= 0 || player2.health <= 0) ||
-            checkTrailCollision(player1, player2.trail) || checkTrailCollision(player2, player1.trail) ||
-            checkCollision(player1.rect, player2.rect)) {
-            quit = true;
+            // Check for collisions
+            if (player1.health <= 0 || player2.health <= 0 ||
+                checkTrailCollision(player1, player2.trail) || checkTrailCollision(player2, player1.trail) ||
+                checkCollision(player1.rect, player2.rect)) {
+                gameState = GAMEOVER;
+            }
+
+            // Handle player input
+            const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
+            if (keyboardState[SDL_SCANCODE_W] && player1.direction.y == 0) player1.direction = { 0, -1 };
+            if (keyboardState[SDL_SCANCODE_S] && player1.direction.y == 0) player1.direction = { 0, 1 };
+            if (keyboardState[SDL_SCANCODE_A] && player1.direction.x == 0) player1.direction = { -1, 0 };
+            if (keyboardState[SDL_SCANCODE_D] && player1.direction.x == 0) player1.direction = { 1, 0 };
+
+            if (keyboardState[SDL_SCANCODE_UP] && player2.direction.y == 0) player2.direction = { 0, -1 };
+            if (keyboardState[SDL_SCANCODE_DOWN] && player2.direction.y == 0) player2.direction = { 0, 1 };
+            if (keyboardState[SDL_SCANCODE_LEFT] && player2.direction.x == 0) player2.direction = { -1, 0 };
+            if (keyboardState[SDL_SCANCODE_RIGHT] && player2.direction.x == 0) player2.direction = { 1, 0 };
         }
-
-        // Handle player input
-        const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
-        if (keyboardState[SDL_SCANCODE_W] && player1.direction.y == 0) player1.direction = { 0, -1 };
-        if (keyboardState[SDL_SCANCODE_S] && player1.direction.y == 0) player1.direction = { 0, 1 };
-        if (keyboardState[SDL_SCANCODE_A] && player1.direction.x == 0) player1.direction = { -1, 0 };
-        if (keyboardState[SDL_SCANCODE_D] && player1.direction.x == 0) player1.direction = { 1, 0 };
-
-        if (keyboardState[SDL_SCANCODE_UP] && player2.direction.y == 0) player2.direction = { 0, -1 };
-        if (keyboardState[SDL_SCANCODE_DOWN] && player2.direction.y == 0) player2.direction = { 0, 1 };
-        if (keyboardState[SDL_SCANCODE_LEFT] && player2.direction.x == 0) player2.direction = { -1, 0 };
-        if (keyboardState[SDL_SCANCODE_RIGHT] && player2.direction.x == 0) player2.direction = { 1, 0 };
 
         // Clear the renderer
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Draw player trails
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-        for (const auto& rect : player1.trail) {
-            SDL_RenderFillRect(renderer, &rect);
-        }
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        for (const auto& rect : player2.trail) {
-            SDL_RenderFillRect(renderer, &rect);
-        }
+        if (gameState == MENU) {
+            // Render the start menu
+            renderStartMenu(renderer, font);
+        } else if (gameState == PLAYING) {
+            // Draw player trails
+            SDL_SetRenderDrawColor(renderer, player1.color.r, player1.color.g, player1.color.b, player1.color.a);
+            for (const auto& rect : player1.trail) {
+                SDL_RenderFillRect(renderer, &rect);
+            }
 
-        // Draw players
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-        SDL_RenderFillRect(renderer, &player1.rect);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &player2.rect);
+            SDL_SetRenderDrawColor(renderer, player2.color.r, player2.color.g, player2.color.b, player2.color.a);
+            for (const auto& rect : player2.trail) {
+                SDL_RenderFillRect(renderer, &rect);
+            }
+
+            // Draw players
+            SDL_SetRenderDrawColor(renderer, player1.color.r, player1.color.g, player1.color.b, player1.color.a);
+            SDL_RenderFillRect(renderer, &player1.rect);
+
+            SDL_SetRenderDrawColor(renderer, player2.color.r, player2.color.g, player2.color.b, player2.color.a);
+            SDL_RenderFillRect(renderer, &player2.rect);
+        } else if (gameState == PAUSED) {
+            // Render the pause menu
+            renderPauseMenu(renderer, font);
+        } else if (gameState == GAMEOVER) {
+            // Render game over screen
+            SDL_Color color = { 255, 255, 255, 255 };
+            renderText(renderer, "GAME OVER", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 50, color, font);
+            renderText(renderer, "Press SPACE to Restart", WINDOW_WIDTH / 2 - 150, WINDOW_HEIGHT / 2, color, font);
+        }
 
         // Present the renderer
         SDL_RenderPresent(renderer);
@@ -155,8 +232,11 @@ int main(int argc, char* argv[]) {
         SDL_Delay(16);
     }
 
+    // Cleanup
+    TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
 
     return 0;
